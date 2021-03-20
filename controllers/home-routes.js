ba0a3a5ac67d07e1ac8f130aa664e263df282892
Jesus <jarellano562@yahoo.com>
2021-03-20 16:51:39 -0700

    using the blog app for my routes to see if they work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 //GET====================================================================================
 router.get('/', (req, res) => {
