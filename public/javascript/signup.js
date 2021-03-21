5d05f93bdc628aae7d56b4b7e9f65152e16220dd
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-03-21 16:59:25 -0700

    fmapped out layouts and content for front end

---
+++ public/javascript/signup.js
@@ -0,0 +1,29 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const permission = document.querySelector('#permission-signup');
+  
+    if (username && email && password && permission) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+          permission
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
