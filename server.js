98689f46f2383430fe4c2a8d16022b556215239d
Hayden <haydenkd@protonmail.com>
2021-03-24 15:41:09 -0700

    commit

--- server.js
+++ server.js
@@ -39,6 +39,14 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
+async function checkFlyers(){
+    while (true){
+        
+    }
+}
+
+checkFlyers();
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(' Now listening ' + PORT));
 });
