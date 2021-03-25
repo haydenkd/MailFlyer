d55b0cf0b4894252b8d469968a94aa73e0df12d1
Brandon <brandon.eg@gmail.com>
2021-03-24 19:21:46 -0700

    fixed create flyer database null values

--- public/javascript/add-flyer.js
+++ public/javascript/add-flyer.js
@@ -5,6 +5,20 @@ async function newFormHandler(event) {
   const type = document.querySelector('#flyerChoice').value;
   const frequency = document.querySelector('#duration').value;
 
+  sessionStorage.setItem("recipient", recipient)
+  sessionStorage.setItem("type", type)
+  sessionStorage.setItem("frequency", frequency)
+
+  console.log("\n\n");
+  console.log("\n\n");
+  console.log("\n\n");
+  console.log(recipient);
+  console.log(type);
+  console.log(frequency);
+  console.log("\n\n");
+  console.log("\n\n");
+  console.log("\n\n");
+
   const response = await fetch(`/api/flyer`, {
     method: 'POST',
     body: JSON.stringify({
