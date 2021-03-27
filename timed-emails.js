50a742a1ada8f903a044fc9e8afa87e25cfdd55a
Hayden <haydenkd@protonmail.com>
2021-03-26 18:24:13 -0700

    update

--- timed-emails.js
+++ timed-emails.js
@@ -1,6 +1,7 @@
 const sequelize = require('./config/connection');
 const { Flyer, User, ContentType } = require('./models/');
 const { insultFlyer, jokeFlyer, quoteFlyer} = require('./controllers/mailer/mailer');
+const d = new Date();
 
 function checkEmails(){
     Flyer.findAll({
@@ -24,6 +25,8 @@ function checkEmails(){
         console.log(dbFlyerData[2].dataValues.recipient);
 
         for (let i = 0; i < dbFlyerData.length; i++){
+            // check to see if the last send date is past the frequency time
+            // if(dbFlyerData[i].dataValues)
             if(dbFlyerData[i].dataValues.content_id === 1){
                 quoteFlyer(dbFlyerData[i].dataValues.recipient);
             }
@@ -35,6 +38,14 @@ function checkEmails(){
             if(dbFlyerData[i].dataValues.content_id === 3){
                 insultFlyer(dbFlyerData[i].dataValues.recipient);
             }
+            // set the last send date to today
+            // Flyer.update({
+            //     prev_flyer_date: `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
+            //   }, {
+            //     where: {
+            //       id: dbFlyerData[i].dataValues.id
+            //     }
+            // })
         }
     })
     .catch(err => {
