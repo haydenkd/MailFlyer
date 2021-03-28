7aea255187ae290d6160f6144782e6cebfa31dd5
Hayden <haydenkd@protonmail.com>
2021-03-28 05:32:07 -0700

    fix guard clause

--- timed-emails.js
+++ timed-emails.js
@@ -7,7 +7,7 @@ function checkDate(frequency, lastDateString){
     let lastDay = (parseInt(lastDateString[8]) * 10) + parseInt(lastDateString[9]);
     let lastMonth = (parseInt(lastDateString[5]) * 10) + parseInt(lastDateString[6]);
     if (d.getMonth() === lastMonth){
-        return lastDay > parseInt(frequency);
+        return (d.getDate() - lastDay) > parseInt(frequency);
     }
 
     if (lastMonth === 02){ // 28 day month previously
